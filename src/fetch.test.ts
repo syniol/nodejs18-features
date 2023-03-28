6965d2cfcbc45ee7bf8d36945e17ea58b3228aa3
Hadi T <tajallaei@gmail.com>
2023-03-28 21:42:50 +0100

    minor change

--- src/fetch.test.ts
+++ src/fetch.test.ts
@@ -4,9 +4,6 @@ import assert from "node:assert";
 describe("test fetch", function() {
   test("should get json from http call GET", async () => {
     const res = await fetch('https://nodejs.org/api/documentation.json');
-    if (res.ok) {
-      const data = await res.json();
-    }
 
     assert.equal(
       res.status,
