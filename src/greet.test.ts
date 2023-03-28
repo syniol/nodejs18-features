6f2082f3bfbb30c3da2c1c4290c98a9fc259a4cc
Hadi T <tajallaei@gmail.com>
2023-03-28 12:02:19 +0100

    created an initail test with new nodejs test runner

---
+++ src/greet.test.ts
@@ -0,0 +1,6 @@
+import test from "node:test";
+import assert from "assert";
+
+test("Initial Unit Test with Node 18 test runner", () => {
+    assert.deepEqual("hadi", "hadi", "names are not equal")
+})
